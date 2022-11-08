4c52f021af9e123cb098f60ff674f38fe7a398a3
정윤 <wjddbs1589@naver.com>
2022-11-08 18:02:15 +0900

    11-8. 몬스터 스포너, 패트롤지점 생성, 몬스터 코드 제작중
    기본, 이동, 죽음 완료 추격,인식,공격 기능은 제작예정(주변 콜라이더 탐색, 콜라이더를 향해 ray발사 하여 시야내 플레이어 탐색 및 벽구분 예정)

--- Assets/Scripts/Monster/CrabMonster.cs
+++ Assets/Scripts/Monster/CrabMonster.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
-enum monsterState
+
+public enum monsterState
 {
     idle=0,
     move,
@@ -16,11 +17,144 @@ enum monsterState
 public class CrabMonster : MonoBehaviour
 {
     NavMeshAgent agent;
-    Transform destination;
+    Animator anim;
+    //-----------------------------------
+    float monsterHP = 50; 
+    public float HP
+    {
+        get => monsterHP;
+        set
+        {
+            monsterHP = value;
+            if (monsterHP <= 0.0)
+            {
+                changeMonsterHP?.Invoke();
+            }
+        }
+    }
+    public System.Action changeMonsterHP;
+    //----------------------------------
+    monsterState monsterState;
+    public monsterState MonsterState
+    {
+        get => monsterState;
+        set
+        {
+            switch (value)
+            {
+                case monsterState.idle:
+                    break;
+                case monsterState.move:
+                    break;
+                case monsterState.recognition:
+                    break;
+                case monsterState.chase:
+                    break;
+                case monsterState.attack:
+                    break;
+                case monsterState.die:
+                    break;
+                default:
+                    break;
+            }
+            monsterState = value;
+        }
+    }
+
+    bool staying = false; // 머무르는 코루틴이 여러번 실행되는것 방지
+    //-------------------------------------------
+    float attackCoolTimeOrigin = 3.0f;
+    float attackCoolTime = 0.0f;   
+
+    //-------------------------------------------
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        destination = GameObject.Find("destination").transform;
-        agent.destination = destination.position;
+        anim = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        changeMonsterHP += monsterDead;
+        MonsterState = monsterState.idle;
+    }
+
+    private void monsterDead()
+    {
+        if (GameManager.Inst.MonsterDead == false)
+        {
+            MonsterState = monsterState.die;
+            GameManager.Inst.MonsterDead = true;
+            Destroy(gameObject);
+        }
+            
+    }
+    private void Update()
+    {
+        if (agent.pathPending) // navmesh가 도중에 경로를 재탐색 하여 거리가 0이 되는 경우를 방지. 경로가 계산될때까지 호출 무시
+            return;
+        switch (MonsterState)
+        {
+            case monsterState.idle: // 패트롤 지점에 도착 했을때
+                if (!staying)
+                {
+                    staying = true;
+                    StartCoroutine(stay());
+                }                
+                break;
+            case monsterState.move: // 패트롤 지점으로 이동                
+                Debug.Log(agent.remainingDistance);
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    Debug.Log("도착");
+                    MonsterState = monsterState.idle;
+                }  
+                break;
+            case monsterState.recognition: // 플레이어를 찾았을때
+                int IntimidateType = Random.Range(1, 4);
+                anim.SetInteger("FindPlayer", IntimidateType);
+                MonsterState = monsterState.chase;
+                break;
+            case monsterState.chase: //플레이어 추격
+                anim.SetInteger("FindPlayer", 0);
+                agent.SetDestination(GameManager.Inst.Player.transform.position);
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                { 
+                    MonsterState = monsterState.attack;
+                }
+                break;
+            case monsterState.attack:
+                if(attackCoolTime <= 0)
+                {
+                    int AttackType = Random.Range(1, 6);
+                    anim.SetInteger("FindPlayer", AttackType);
+                    attackCoolTime = attackCoolTimeOrigin;
+                }
+                else
+                {
+                    attackCoolTime -= Time.deltaTime;
+                }                
+                break;
+            case monsterState.die:
+                anim.SetTrigger("Die");
+                break;
+            default:
+                break;
+        }        
+    }
+
+    IEnumerator stay()
+    {
+        anim.SetBool("Moving", false);
+        yield return new WaitForSeconds(5.0f);
+        if(MonsterState == monsterState.idle)
+        {            
+            MonsterState = monsterState.move;
+            anim.SetBool("Moving", true);
+            int randNum = Random.Range(1, 23);
+            Transform destination = GameManager.Inst.MonsterSpawner.spawnPos[randNum];
+            Debug.Log(destination.position);
+            agent.SetDestination(destination.position);
+            staying = false;
+        }
     }
 }
---
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public Transform[] spawnPos;
+    public GameObject monsterPrefab;
+    private void Awake()
+    {
+        spawnPos = GetComponentsInChildren<Transform>();
+    }
+    private void Start()
+    {
+        spawnMonster();
+    }
+    public void spawnMonster()
+    {
+        int rand = Random.Range(1, 23);
+        Instantiate(monsterPrefab, spawnPos[rand]);           
+    }
+}
